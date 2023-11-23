41683d8fdf768ef54b80b5254361033b384e07fb
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-11-23 16:38:14 +0100

    navbar et footer

---
+++ include/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/css/uikit.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.11/dist/js/uikit-icons.min.js"></script>
+    <title>DarkSanta</title>
+</head>
+<body>
+<footer>
+	<p>&#169;DarkSanta |
+		<a href="#">Mentions légales</a>
+	</p>
+	<ul>
+		<li><a href="https://www.leetchi.com/fr/c/votre-argent-contre-mes-cadeaux-1876355"><img src="/images/leetchi.svg" alt="Leetchi"></a></li>
+		<li><a href="https://www.paypal.com/fr/home"><img src="/images/paypal.svg" alt="Paypal"></a></li>
+		<li><a href="https://onlyfans.com/u378558738">Mon lien secret</a></li>
+	</ul>
+</footer>
+</body>
+</html>
